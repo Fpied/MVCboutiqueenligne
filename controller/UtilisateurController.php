b678965c16f9f3a50583816de4d962fb17c27945
Simon-luc <Simon_luc85@hotmail.com>
2026-07-01 18:15:36 +0200

    Ajout/implementation du UtilisateurController

---
+++ controller/UtilisateurController.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__ . "/../model/Utilisateur.php";
+
+class UtilisateurController
+{
+    public function login()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+            $userModel = new Utilisateur();
+            $user = $userModel->getByEmail($email);
+
+            if ($user && password_verify($password, $user['password_hash'])) {
+                if ($user['role'] === 'admin') {
+                    $_SESSION['admin'] = true;
+                }
+                $_SESSION['user'] = $user['id'];
+                header("Location: index.php");
+                exit();
+            }
+        }
+
+        require __DIR__ . "/../view/login.php";
+    }
+}
+?>
