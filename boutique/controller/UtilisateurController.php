a640290d8c939f9c75ca835af955ee3baf15ed9c
Simon-luc <Simon_luc85@hotmail.com>
2026-07-03 12:15:52 +0200

    Refactor UtilisateurController (login GET+POST)

--- boutique/controller/UtilisateurController.php
+++ boutique/controller/UtilisateurController.php
@@ -5,38 +5,48 @@ require_once __DIR__ . "/../repository/UtilisateurRepository.php";
 
 class UtilisateurController
 {
+    private UtilisateurRepository $repo;
+
+    public function __construct()
+    {
+        session_start();
+        $this->repo = new UtilisateurRepository();
+    }
+
     public function login()
     {
-        $error = "";
+            $error = "";
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $email = $_POST['email'];
-            $password = $_POST['password'];
 
-            $userModel = new UtilisateurRepository();
-            $user = $userModel->getByEmail($email);
+            $email = $_POST['email'] ?? '';
+            $password = $_POST['password'] ?? '';
 
-            if ($user && password_verify($password, $user['password_hash'])) {
-                if ($user['role'] === 'admin') {
+            $user = $this->repo->getByEmail($email);
+
+            if ($user && password_verify($password, $user->password_hash)) {
+
+
+                if ($user->role === 'admin') {
                     $_SESSION['admin'] = true;
                 }
 
-                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['user_id'] = $user->id;
+                $_SESSION['user_name'] = $user->name;
+                $_SESSION['user_email'] = $user->email;
 
                 header("Location: index.php");
                 exit();
-            }
+    }}}
 
-            $error = "Email ou mot de passe incorrect";
-        }
-
-        require __DIR__ . "/../view/login.php";
-    }
 
     public function logout()
     {
+        session_start();
         session_destroy();
         header("Location: index.php");
         exit();
     }
 }
+
+?>
