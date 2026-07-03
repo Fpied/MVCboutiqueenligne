1313ce16138a64eb8f37452ca801e5a7a28a7c56
Simon-luc <Simon_luc85@hotmail.com>
2026-07-03 11:09:22 +0200

    ajout logout dans UtilisateurController

--- controller/UtilisateurController.php
+++ controller/UtilisateurController.php
@@ -1,38 +1,49 @@
 <?php
 require_once __DIR__ . "/../model/Utilisateur.php";
+require_once __DIR__ . "/../repository/UtilisateurRepository.php";
 
 class UtilisateurController
 {
-    public function login()
+    private UtilisateurRepository $repository;
+
+    public function __contrct(PDO $pdo)
     {
+        $this->repository = new UtilisateurRepository($pdo);
+
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
-
-
+    }
+    public function login()
+    {
+        $error = "";
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $email = $_POST['email'];
             $password = $_POST['password'];
 
+            $user = $this->repository->getByEmail($email);
 
-            $userModel = new Utilisateur();
 
-            $userModel = new UtilisateurRepository();
+            if ($user && password_verify($password, $user->getPassword_hash())) {
 
-            $user = $userModel->getByEmail($email);
-
-            if ($user && password_verify($password, $user['password_hash'])) {
-                if ($user['role'] === 'admin') {
+                if ($user->getRole() === 'admin') {
                     $_SESSION['admin'] = true;
                 }
-                $_SESSION['user'] = $user['id'];
+
+                $_SESSION['user'] = $user->getId();
                 header("Location: index.php");
                 exit();
             }
+            require __DIR__ . "/../view/login.php";
         }
-
-        require __DIR__ . "/../view/login.php";
+    }
+    public function logout()
+    {
+        session_destroy();
+        header("Location: index.php");
+        exit();
     }
 }
+
 ?>
